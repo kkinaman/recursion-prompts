d114fd4709e5b0e199edaa6066141cc57a957d1a
Karina Kinaman <kkinaman@alumni.virginia.edu>
2016-09-22 19:54:25 -0700

    Implement mulitply

--- src/recursion.js
+++ src/recursion.js
@@ -179,6 +179,14 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator  or
 // JavaScript's Math object.
 var multiply = function(x, y) {
+  if (y === 0) {
+    return 0;
+  }
+  if ((x > 0 && y > 0) || (x <= 0 && y > 0)) {
+    return x + multiply(x, y - 1);
+  } else if ((x < 0 && y < 0) || (x > 0 && y < 0)) {
+    return multiply(x, y + 1) - x;
+  }
 };
 
 // 13. Write a function that divides two numbers without using the / operator  or
